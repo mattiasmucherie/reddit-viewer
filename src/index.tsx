2d09842caa2ef7a3c703f05d929bf9b69215b25b
Mattias Mucherie <mattias.mattias.m@gmail.com>
2021-07-06 11:37:21 +0200

    Added logrocket

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import LogRocket from 'logrocket'
+import './index.css'
+import App from './App'
 
+LogRocket.init('muodip/reddit-viewer')
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
